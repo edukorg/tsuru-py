00ead0c45132d60d8a91e8e8a4ddc39d8c125111
João Daher <joao@daher.dev>
2019-06-28 18:24:54 -0300

    Add support to Deploy resource

--- tsuru/__init__.py
+++ tsuru/__init__.py
@@ -1,5 +1,6 @@
 from tsuru.models import (
     App,
+    Deploy,
 )
 from tsuru.exceptions import (
     DoesNotExist,
@@ -13,6 +14,7 @@ version = '0.1.0'
 
 __all__ = (
     'App',
+    'Deploy',
     'DoesNotExist',
     'UnexpectedDataFormat',
     'UnsupportedModelException',
--- tsuru/models/__init__.py
+++ tsuru/models/__init__.py
@@ -1,4 +1,6 @@
 from tsuru.models.app import App
+from tsuru.models.deploy import Deploy
 __all__ = (
     'App',
+    'Deploy',
 )
